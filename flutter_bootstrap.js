8c927905c263eb66854887d100f1cbdc729ece53
Deploy Script <deploy@example.com>
2025-05-18 11:53:45 +0800

    0518更新

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1530361035"
+    serviceWorkerVersion: "3974295427"
   }
 });
